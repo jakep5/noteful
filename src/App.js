80dc26bec6cfb84d49b2774539d29ca25ee37f65
jakep5 <jakepagel1@yahoo.com>
2019-10-24 15:34:20 -0500

    add NotesContext and provider components

--- src/App.js
+++ src/App.js
@@ -3,38 +3,43 @@ import { Route, Switch } from 'react-router-dom';
 import MainPage from './MainPage/MainPage';
 import FolderPage from './FolderPage/FolderPage';
 import NotePage from './NotePage/NotePage';
+import NotesContext from './NotesContext/NotesContext';
 
 
-function App() {
-  return (
-    <main className='App'>
-      <Switch>
-        <Route 
-          exact path="/" 
-          render = {({history}) => {
-            return(
-              <MainPage  
-                onClickTitle = {() => history.push("/")}/>
-          )}} />
-        <Route 
-          path="/folders/:folderId" 
-          render = {({history}) => {
-            return (
-              <FolderPage 
-                onClickTitle = {() => history.push("/")}/>
-            )}} />
-        <Route 
-          path="/notes/:noteId" 
-          render = {({history}) => {
-            return (
-              <NotePage 
-                onClickBack = {() => history.goBack()} 
-                onClickTitle = {() => history.push("/")}/>
-            )
-          }}/>
-      </Switch>
-    </main>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <main className='App'>
+        <NotesContext.Provider>
+          <Switch>
+            <Route 
+              exact path="/" 
+              render = {({history}) => {
+                return(
+                  <MainPage  
+                    onClickTitle = {() => history.push("/")}/>
+              )}} />
+            <Route 
+              path="/folders/:folderId" 
+              render = {({history}) => {
+                return (
+                  <FolderPage 
+                    onClickTitle = {() => history.push("/")}/>
+                )}} />
+            <Route 
+              path="/notes/:noteId" 
+              render = {({history}) => {
+                return (
+                  <NotePage 
+                    onClickBack = {() => history.goBack()} 
+                    onClickTitle = {() => history.push("/")}/>
+                )
+              }}/>
+          </Switch>
+        </NotesContext.Provider>
+      </main>
+    );
+  }
 }
 
 export default App;
